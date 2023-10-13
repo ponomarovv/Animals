c369fcef55f81d417100783894edc1218399fa48
Ponomarov Oleksii <testdrive819@gmail.com>
2023-10-13 19:08:35 +0300

    implemented get ping, get dogs

--- Animals.API/Controllers/DogController.cs
+++ Animals.API/Controllers/DogController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Animals.DAL.Abstract.Repository.Base;
+using Animals.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Animals.API.Controllers
@@ -7,5 +9,28 @@ namespace Animals.API.Controllers
     [ApiController]
     public class DogController : ControllerBase
     {
+        private readonly IUnitOfWork _uow;
+
+        public DogController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+
+        [HttpGet("/ping")]
+        public ActionResult<string> Ping()
+        {
+            var result = "Dogshouseservice.Version1.0.1";
+            return Ok(result);
+        }
+
+        [HttpGet("/dogs")]
+        public async Task<ActionResult<List<Dog>>> GetAll()
+        {
+            var result = await _uow.DogRepository.GetAllAsync(x => true);
+            if (result.Count == 0) return BadRequest("There are no dogs in database");
+
+            return Ok(result);
+        }
     }
 }
